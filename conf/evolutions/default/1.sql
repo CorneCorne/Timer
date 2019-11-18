5c89be22a44d966e1af2fa01f3987008883d2e5d
Sugita Shiyunya <sugita.s.ae@cs-mac78.edu.c.titech.ac.jp>
2019-11-18 14:51:01 +0900

    クラスにあわせてSQLの変数名等を変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -11,13 +11,26 @@ CREATE TABLE enquete (
 
 CREATE TABLE task (
     id int PRIMARY KEY AUTO_INCREMENT,
+    task_id varchar(32) NOT NULL,
+    account_id varchar(32) NOT NULL,
     title varchar(32) NOT NULL,
     description varchar(255),
     is_done boolean default FALSE,
     created_at timestamp default CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE account (
+    id int AUTO_INCREMENT,
+    account_id varchar(32) PRIMARY KEY NOT NULL,
+    user_name varchar(255) NOT NULL,
+    account_password varchar(255) NOT NULL,
+    session_id varchar(255),
+    session_timestamp timestamp
+);
+
 # --- !Downs
 DROP TABLE enquete;
 
 DROP TABLE task;
+
+DROP TABLE account;
